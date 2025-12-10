6d7e1586aca55ecac61fa0d0aef621d6c3a15e07
CTTFelipeGomes <cttfelipegomes@gmail.com>
2025-12-10 00:35:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const res = document.querySelector(".res");
+
+function garcom() {
+    let ramdom = Math.floor(Math.random() * 1026); // corrigido
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${ramdom}`)
+        .then((response) => response.json())
+        .then((data) => {
+            gerarPokemon(data);
+        })
+        .catch((erro) => console.log("Erro:", erro));
+}
+
+function gerarPokemon(data) {
+    let nome = data.name;
+    let id = data.id;
+    let tipo = data.types[0].type.name;
+    let img = data.sprites.front_default;
+
+    res.innerHTML = `
+        <div class="card">
+            <img src="${img}" alt="${nome}">
+            <div class="container">
+                <div class="title">
+                    <h1>${nome}</h1>
+                    <p>nº ${id}</p>
+                </div>
+                <p id="poison">${tipo}</p>
+            </div>
+        </div>
+    `;
+}
